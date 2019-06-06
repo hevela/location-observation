0e44e4c7173048b9504ecb47f31f099e8a8adc95
Hector Vela <hector.vela@wizeline.com>
2019-06-06 15:28:27 -0500

    prepend slashes

--- packages/backend/locations/router.js
+++ packages/backend/locations/router.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.get('/', location.getAllLocations);
 app.post('/', location.createLocation);
-app.get('/:id', location.getLocation);
-app.put('/:id', location.updateLocation);
-app.delete('/:id', location.deleteLocation);
+app.get('/:id/', location.getLocation);
+app.put('/:id/', location.updateLocation);
+app.delete('/:id/', location.deleteLocation);
 
 export default app;
