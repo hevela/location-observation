051d3da8c145675afb89162dc8de05b2f14aa727
Hector Vela <hector.vela@wizeline.com>
2019-06-09 13:35:37 -0500

    create location store stub

---
+++ packages/frontend/tests/stubs/locationsStore.js
@@ -0,0 +1,17 @@
+export default {
+  namespaced: true,
+  state: {
+    createLocationSuccess: false,
+    createLocationFailure: false,
+    createLocationInProgress: false,
+    createLocationError: '',
+    updateLocationSuccess: false,
+    updateLocationFailure: false,
+    updateLocationInProgress: false,
+    updateLocationError: '',
+    deleteLocationSuccess: false,
+    deleteLocationFailure: false,
+    deleteLocationInProgress: false,
+    deleteLocationError: '',
+  },
+};
